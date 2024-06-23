304f14862129b3d0e1a810a67c9189036d8a37ed
lalaalal <bontin0128@gmail.com>
2024-06-23 20:54:25 +0900

    Fix hanging sign gui

--- src/main/java/com/yummy/naraka/world/block/entity/NarakaHangingSignBlockEntity.java
+++ src/main/java/com/yummy/naraka/world/block/entity/NarakaHangingSignBlockEntity.java
@@ -1,32 +1,24 @@
 package com.yummy.naraka.world.block.entity;
 
 import net.minecraft.core.BlockPos;
-import net.minecraft.sounds.SoundEvent;
-import net.minecraft.sounds.SoundEvents;
+import net.minecraft.world.item.DyeColor;
 import net.minecraft.world.level.block.entity.BlockEntityType;
+import net.minecraft.world.level.block.entity.HangingSignBlockEntity;
+import net.minecraft.world.level.block.entity.SignText;
 import net.minecraft.world.level.block.state.BlockState;
 
-public class NarakaHangingSignBlockEntity extends NarakaSignBlockEntity {
-    public NarakaHangingSignBlockEntity(BlockEntityType type, BlockPos pos, BlockState blockState) {
-        super(type, pos, blockState);
-    }
-
+public class NarakaHangingSignBlockEntity extends HangingSignBlockEntity {
     public NarakaHangingSignBlockEntity(BlockPos pos, BlockState blockState) {
-        this(NarakaBlockEntities.EBONY_HANGING_SIGN.get(), pos, blockState);
+        super(pos, blockState);
     }
 
     @Override
-    public int getTextLineHeight() {
-        return 9;
+    public BlockEntityType<?> getType() {
+        return NarakaBlockEntities.EBONY_HANGING_SIGN.get();
     }
 
     @Override
-    public int getMaxTextLineWidth() {
-        return 60;
-    }
-
-    @Override
-    public SoundEvent getSignInteractionFailedSoundEvent() {
-        return SoundEvents.WAXED_HANGING_SIGN_INTERACT_FAIL;
+    protected SignText createDefaultSignText() {
+        return super.createDefaultSignText().setColor(DyeColor.WHITE);
     }
 }
