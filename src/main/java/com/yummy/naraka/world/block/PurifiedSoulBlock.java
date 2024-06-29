984820a57b889d2e0d69056ed07f76b1c117daa9
lalaalal <bontin0128@gmail.com>
2024-06-29 19:21:34 +0900

    Fix leaves item model, ebony flammability, translations

---
+++ src/main/java/com/yummy/naraka/world/block/PurifiedSoulBlock.java
@@ -0,0 +1,28 @@
+package com.yummy.naraka.world.block;
+
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.ItemInteractionResult;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.phys.BlockHitResult;
+
+public class PurifiedSoulBlock extends Block {
+    public PurifiedSoulBlock(Properties properties) {
+        super(properties);
+    }
+
+    @Override
+    protected ItemInteractionResult useItemOn(ItemStack itemStack, BlockState state, Level level, BlockPos pos, Player player, InteractionHand hand, BlockHitResult hitResult) {
+        if (itemStack.is(Items.FLINT_AND_STEEL) && hitResult.getDirection() == Direction.UP) {
+            level.setBlock(pos.above(), NarakaBlocks.PURIFIED_SOUL_FIRE_BLOCK.get().defaultBlockState(), 10);
+            return ItemInteractionResult.SUCCESS;
+        }
+        return super.useItemOn(itemStack, state, level, pos, player, hand, hitResult);
+    }
+}
