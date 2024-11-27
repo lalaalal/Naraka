b3fdb909e07ab9cf8615472af04a88f6889fd870
lalaalal <bontin0128@gmail.com>
2024-11-27 09:55:44 +0900

    Refactoring SoulType constructor

--- common/src/main/java/com/yummy/naraka/world/item/SoulType.java
+++ common/src/main/java/com/yummy/naraka/world/item/SoulType.java
@@ -1,10 +1,12 @@
 package com.yummy.naraka.world.item;
 
 import com.mojang.serialization.Codec;
-import com.yummy.naraka.world.block.NarakaBlocks;
+import com.yummy.naraka.NarakaMod;
 import io.netty.buffer.ByteBuf;
+import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.network.codec.ByteBufCodecs;
 import net.minecraft.network.codec.StreamCodec;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.util.ByIdMap;
 import net.minecraft.util.StringRepresentable;
 import net.minecraft.world.item.Item;
@@ -12,26 +14,29 @@ import net.minecraft.world.item.ItemStack;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.function.IntFunction;
-import java.util.function.Supplier;
 
 public enum SoulType implements StringRepresentable {
-    REDSTONE(0, () -> NarakaItems.SOUL_INFUSED_REDSTONE.get(), () -> NarakaBlocks.SOUL_INFUSED_REDSTONE_BLOCK.get().asItem(), 0xeb4747),
-    COPPER(1, () -> NarakaItems.SOUL_INFUSED_COPPER.get(), () -> NarakaBlocks.SOUL_INFUSED_COPPER_BLOCK.get().asItem(), 0xff8000),
-    GOLD(2, () -> NarakaItems.SOUL_INFUSED_GOLD.get(), () -> NarakaBlocks.SOUL_INFUSED_GOLD_BLOCK.get().asItem(), 0xffd24d),
-    EMERALD(3, () -> NarakaItems.SOUL_INFUSED_EMERALD.get(), () -> NarakaBlocks.SOUL_INFUSED_EMERALD_BLOCK.get().asItem(), 0x0ec70e),
-    DIAMOND(4, () -> NarakaItems.SOUL_INFUSED_DIAMOND.get(), () -> NarakaBlocks.SOUL_INFUSED_DIAMOND_BLOCK.get().asItem(), 0x33cccc),
-    LAPIS(5, () -> NarakaItems.SOUL_INFUSED_LAPIS.get(), () -> NarakaBlocks.SOUL_INFUSED_LAPIS_BLOCK.get().asItem(), 0x3939c6),
-    AMETHYST(6, () -> NarakaItems.SOUL_INFUSED_AMETHYST.get(), () -> NarakaBlocks.SOUL_INFUSED_AMETHYST_BLOCK.get().asItem(), 0x9957db),
-    NECTARIUM(7, () -> NarakaItems.SOUL_INFUSED_NECTARIUM.get(), () -> NarakaBlocks.SOUL_INFUSED_NECTARIUM_BLOCK.get().asItem(), 0xd65cd6),
-    GOD_BLOOD(8, () -> NarakaItems.GOD_BLOOD.get(), () -> NarakaItems.GOD_BLOOD.get(), 0x625859);
+    REDSTONE(0, 0xeb4747),
+    COPPER(1, 0xff8000),
+    GOLD(2, 0xffd24d),
+    EMERALD(3, 0x0ec70e),
+    DIAMOND(4, 0x33cccc),
+    LAPIS(5, 0x3939c6),
+    AMETHYST(6, 0x9957db),
+    NECTARIUM(7, 0xd65cd6),
+    GOD_BLOOD(8, "god_blood", "god_blood", 0x625859);
 
     private static final IntFunction<SoulType> BY_ID = ByIdMap.continuous(SoulType::getId, SoulType.values(), ByIdMap.OutOfBoundsStrategy.ZERO);
     public static final Codec<SoulType> CODEC = StringRepresentable.fromEnum(SoulType::values);
     public static final StreamCodec<ByteBuf, SoulType> STREAM_CODEC = ByteBufCodecs.idMapper(BY_ID, SoulType::getId);
 
     public final int id;
-    private final Supplier<Item> soulItem;
-    private final Supplier<Item> soulBlock;
+    private final ResourceLocation itemName;
+    private final ResourceLocation blockItemName;
+    @Nullable
+    private Item item;
+    @Nullable
+    private Item blockItem;
     public final int color;
     public final String translationKey;
 
@@ -44,12 +49,32 @@ public enum SoulType implements StringRepresentable {
         return null;
     }
 
-    SoulType(int id, Supplier<Item> soulItem, Supplier<Item> soulBlock, int color) {
+    SoulType(int id, String itemName, String blockItemName, int color) {
         this.id = id;
-        this.soulBlock = soulBlock;
-        this.soulItem = soulItem;
+        this.itemName = NarakaMod.location(itemName);
+        this.blockItemName = NarakaMod.location(blockItemName);
+        BuiltInRegistries.ITEM.getOptional(this.itemName)
+                .ifPresent(item -> this.item = item);
+        BuiltInRegistries.ITEM.getOptional(this.blockItemName)
+                .ifPresent(item -> this.blockItem = item);
         this.color = color;
         this.translationKey = "soul_type.naraka." + name().toLowerCase();
+    }
+
+    SoulType(int id, int color) {
+        this.id = id;
+        this.itemName = getDefaultItemName();
+        this.blockItemName = getDefaultBlockItemName();
+        this.color = color;
+        this.translationKey = "soul_type.naraka." + name().toLowerCase();
+    }
+
+    private ResourceLocation getDefaultItemName() {
+        return NarakaMod.location("soul_infused_" + name().toLowerCase());
+    }
+
+    private ResourceLocation getDefaultBlockItemName() {
+        return NarakaMod.location("soul_infused_" + name().toLowerCase() + "_block");
     }
 
     public int getId() {
@@ -61,11 +86,15 @@ public enum SoulType implements StringRepresentable {
     }
 
     public Item getBlockItem() {
-        return soulBlock.get();
+        if (blockItem != null)
+            return blockItem;
+        return blockItem = BuiltInRegistries.ITEM.getOptional(blockItemName).orElseThrow();
     }
 
     public Item getItem() {
-        return soulItem.get();
+        if (item != null)
+            return item;
+        return item = BuiltInRegistries.ITEM.getOptional(itemName).orElseThrow();
     }
 
     public boolean test(ItemStack itemStack) {
