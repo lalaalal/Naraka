8e924933695c1dd01ef661e9df28906f5eb35bee
lalaalal <bontin0128@gmail.com>
2025-04-25 04:20:15 +0900

    fix(jei): Remove jei

--- common/src/main/java/com/yummy/naraka/client/renderer/entity/state/package-info.java
+++ common/src/main/java/com/yummy/naraka/client/renderer/entity/state/package-info.java
@@ -2,7 +2,7 @@
 @FieldsAreNonnullByDefault
 @ParametersAreNonnullByDefault
 
-package com.yummy.naraka.jei;
+package com.yummy.naraka.client.renderer.entity.state;
 
 import net.minecraft.FieldsAreNonnullByDefault;
 import net.minecraft.MethodsReturnNonnullByDefault;
