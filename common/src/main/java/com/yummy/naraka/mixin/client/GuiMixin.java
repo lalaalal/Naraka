6b7731208b33b348599ed05353795341eb90c982
lalaalal <bontin0128@gmail.com>
2025-07-22 02:03:16 +0900

    fix: Heart rendering with locked hearts

--- common/src/main/java/com/yummy/naraka/mixin/client/GuiMixin.java
+++ common/src/main/java/com/yummy/naraka/mixin/client/GuiMixin.java
@@ -1,6 +1,7 @@
 package com.yummy.naraka.mixin.client;
 
 import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
+import com.llamalad7.mixinextras.sugar.Local;
 import com.yummy.naraka.world.entity.data.LockedHealthHelper;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
@@ -16,7 +17,8 @@ import org.spongepowered.asm.mixin.injection.ModifyArg;
 @Environment(EnvType.CLIENT)
 @Mixin(Gui.class)
 public abstract class GuiMixin {
-    @Shadow @Nullable
+    @Shadow
+    @Nullable
     protected abstract Player getCameraPlayer();
 
     @Shadow
@@ -36,12 +38,25 @@ public abstract class GuiMixin {
     @ModifyArg(method = "renderPlayerHealth", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/Gui;renderHearts(Lnet/minecraft/client/gui/GuiGraphics;Lnet/minecraft/world/entity/player/Player;IIIIFIIIZ)V"), index = 5)
     public int modifyOffsetHeartIndex(int original) {
         Player player = getCameraPlayer();
-        if (player != null) {
+        if (player != null && original != -1) {
             double heartCount = player.getMaxHealth() / 2;
             int offsetHeartIndex = this.tickCount % Mth.ceil(heartCount + 5.0F);
             if (offsetHeartIndex > heartCount)
                 return -1;
             return offsetHeartIndex;
+        }
+        return original;
+    }
+
+    @ModifyArg(method = "renderHearts", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/Gui;renderHeart(Lnet/minecraft/client/gui/GuiGraphics;Lnet/minecraft/client/gui/Gui$HeartType;IIZZZ)V"), index = 3)
+    public int modifyRandomValue(int original, @Local(name = "y") int y, @Local(name = "height") int height, @Local(name = "m") int m, @Local(name = "l") int l) {
+        Player player = getCameraPlayer();
+        if (player != null) {
+            int originalHeartCount = Mth.ceil(player.getMaxHealth() + LockedHealthHelper.get(player) / 2);
+            int heartCount = Mth.ceil(player.getMaxHealth() / 2);
+            if (heartCount <= l && l < originalHeartCount)
+                return y - m * height;
+            return original;
         }
         return original;
     }
