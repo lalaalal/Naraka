1ec1c9c750ef9df2f385858cc615ff8e5c1a6be5
lalaalal <bontin0128@gmail.com>
2025-10-24 03:17:10 +0900

    fix: NeoForge nectarium particle spawning

--- common/src/main/java/com/yummy/naraka/client/particle/NectariumParticle.java
+++ common/src/main/java/com/yummy/naraka/client/particle/NectariumParticle.java
@@ -1,33 +1,29 @@
 package com.yummy.naraka.client.particle;
 
-import com.yummy.naraka.NarakaMod;
-import com.yummy.naraka.core.particles.NarakaParticleTypes;
-import com.yummy.naraka.mixin.invoker.DripParticleInvoker;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.minecraft.client.multiplayer.ClientLevel;
 import net.minecraft.client.particle.DripParticle;
 import net.minecraft.client.particle.TextureSheetParticle;
 import net.minecraft.core.particles.SimpleParticleType;
-import net.minecraft.world.level.material.Fluids;
 
 @Environment(EnvType.CLIENT)
 public class NectariumParticle {
     public static TextureSheetParticle createNectariumHangParticle(SimpleParticleType type, ClientLevel level, double x, double y, double z, double xSpeed, double ySpeed, double zSpeed) {
-        DripParticle particle = DripParticleInvoker.DripHangParticleInvoker.create(level, x, y, z, Fluids.EMPTY, NarakaParticleTypes.FALLING_NECTARIUM.get());
+        TextureSheetParticle particle = DripParticle.createHoneyHangParticle(type, level, x, y, z, xSpeed, ySpeed, zSpeed);
         particle.setLifetime(100);
         particle.setColor(0.7f, 0.4f, 0.7f);
         return particle;
     }
 
     public static TextureSheetParticle createNectariumFallParticle(SimpleParticleType type, ClientLevel level, double x, double y, double z, double xSpeed, double ySpeed, double zSpeed) {
-        DripParticle particle = DripParticleInvoker.HoneyFallAndLandParticleInvoker.create(level, x, y, z, Fluids.EMPTY, NarakaParticleTypes.LANDING_NECTARIUM.get());
+        TextureSheetParticle particle = DripParticle.createHoneyFallParticle(type, level, x, y, z, xSpeed, ySpeed, zSpeed);
         particle.setColor(0.7f, 0.4f, 0.7f);
         return particle;
     }
 
     public static TextureSheetParticle createNectariumLandParticle(SimpleParticleType type, ClientLevel level, double x, double y, double z, double xSpeed, double ySpeed, double zSpeed) {
-        DripParticle particle = DripParticleInvoker.DripLandParticleInvoker.create(level, x, y, z, Fluids.EMPTY);
+        TextureSheetParticle particle = DripParticle.createHoneyLandParticle(type, level, x, y, z, xSpeed, ySpeed, zSpeed);
         particle.setLifetime((int) (128 / Math.random() * 0.8 + 0.2));
         particle.setColor(0.7f, 0.4f, 0.7f);
         return particle;
