a91dcd6f505aaa4cf694c139cf41b545c58eb4ab
lalaalal <bontin0128@gmail.com>
2025-11-01 21:50:19 +0900

    feat: Stigma in Jade

--- common/src/main/java/com/yummy/naraka/jade/NarakaEntityDataProvider.java
+++ common/src/main/java/com/yummy/naraka/jade/NarakaEntityDataProvider.java
@@ -5,6 +5,7 @@ import com.yummy.naraka.data.lang.NarakaJadeProviderComponents;
 import com.yummy.naraka.world.entity.data.DeathCountHelper;
 import com.yummy.naraka.world.entity.data.Stigma;
 import com.yummy.naraka.world.entity.data.StigmaHelper;
+import com.yummy.naraka.world.item.NarakaItems;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.Entity;
@@ -13,6 +14,7 @@ import snownee.jade.api.EntityAccessor;
 import snownee.jade.api.IEntityComponentProvider;
 import snownee.jade.api.ITooltip;
 import snownee.jade.api.config.IPluginConfig;
+import snownee.jade.api.ui.IElementHelper;
 
 public class NarakaEntityDataProvider implements IEntityComponentProvider {
     public static final NarakaEntityDataProvider INSTANCE = new NarakaEntityDataProvider();
@@ -24,11 +26,14 @@ public class NarakaEntityDataProvider implements IEntityComponentProvider {
 
     @Override
     public void appendTooltip(ITooltip tooltip, EntityAccessor entityAccessor, IPluginConfig pluginConfig) {
+        IElementHelper elements = IElementHelper.get();
         Entity entity = entityAccessor.getEntity();
         if (entity instanceof LivingEntity livingEntity) {
             Stigma stigma = StigmaHelper.get(livingEntity);
-            if (stigma.value() > 0)
-                tooltip.add(Component.translatable(LanguageKey.JADE_STIGMA_KEY, stigma.value()));
+            if (stigma.value() > 0) {
+                tooltip.add(elements.smallItem(NarakaItems.STIGMA_ROD.get().getDefaultInstance()));
+                tooltip.append(Component.translatable(LanguageKey.JADE_STIGMA_KEY, stigma.value()));
+            }
             if (DeathCountHelper.isDeathCounted(livingEntity))
                 tooltip.add(Component.translatable(LanguageKey.JADE_DEATH_COUNT_KEY, DeathCountHelper.get(livingEntity)));
         }
