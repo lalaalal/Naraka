6dcd4ca84daf565626708845b59941ede575ec30
lalaalal <bontin0128@gmail.com>
2025-11-08 03:26:59 +0900

    feat: SpearOfLonginus creates NarakaPortal

--- common/src/main/java/com/yummy/naraka/world/entity/SpearOfLonginus.java
+++ common/src/main/java/com/yummy/naraka/world/entity/SpearOfLonginus.java
@@ -1,7 +1,11 @@
 package com.yummy.naraka.world.entity;
 
+import com.yummy.naraka.world.NarakaDimensions;
+import com.yummy.naraka.world.block.NarakaBlocks;
 import com.yummy.naraka.world.damagesource.NarakaDamageSources;
 import net.minecraft.ChatFormatting;
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
 import net.minecraft.core.Position;
 import net.minecraft.network.chat.Component;
 import net.minecraft.server.level.ServerLevel;
@@ -11,23 +15,30 @@ import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.phys.BlockHitResult;
 import net.minecraft.world.phys.EntityHitResult;
 import net.minecraft.world.phys.Vec3;
 
 public class SpearOfLonginus extends Spear {
+    private boolean spawnPortal;
+
     protected SpearOfLonginus(EntityType<? extends SpearOfLonginus> entityType, Level level) {
         super(entityType, level);
         setInvulnerable(true);
+        spawnPortal = false;
     }
 
     public SpearOfLonginus(Level level, Position position, ItemStack stack) {
         super(NarakaEntityTypes.THROWN_SPEAR_OF_LONGINUS.get(), level, position, stack);
         setInvulnerable(true);
+        spawnPortal = false;
     }
 
-    public SpearOfLonginus(Level level, LivingEntity owner, ItemStack stack) {
+    public SpearOfLonginus(Level level, LivingEntity owner, ItemStack stack, boolean spawnPortal) {
         super(NarakaEntityTypes.THROWN_SPEAR_OF_LONGINUS.get(), level, owner, stack);
         setInvulnerable(true);
+        this.spawnPortal = spawnPortal;
     }
 
     @Override
@@ -44,6 +55,16 @@ public class SpearOfLonginus extends Spear {
     protected void onHitEntity(EntityHitResult result) {
         super.onHitEntity(result);
         dealtDamage = true;
+    }
+
+    @Override
+    protected void onHitBlock(BlockHitResult result) {
+        super.onHitBlock(result);
+        if (spawnPortal && level().dimension() != NarakaDimensions.NARAKA) {
+            Direction direction = result.getDirection();
+            BlockPos pos = result.getBlockPos();
+            level().setBlock(pos.relative(direction), NarakaBlocks.NARAKA_PORTAL.get().defaultBlockState(), Block.UPDATE_ALL);
+        }
     }
 
     @Override
@@ -70,6 +91,11 @@ public class SpearOfLonginus extends Spear {
         if (!level().isClientSide())
             return 3;
         return 0;
+    }
+
+    @Override
+    public boolean canUsePortal(boolean allowPassengers) {
+        return false;
     }
 
     @Override
