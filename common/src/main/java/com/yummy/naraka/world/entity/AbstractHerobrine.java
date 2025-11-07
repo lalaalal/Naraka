64441cd7a3517559c8ccc800006df502eaa48a6f
lalaalal <bontin0128@gmail.com>
2025-11-08 03:28:23 +0900

    feat: Prevent herobrine using portal

--- common/src/main/java/com/yummy/naraka/world/entity/AbstractHerobrine.java
+++ common/src/main/java/com/yummy/naraka/world/entity/AbstractHerobrine.java
@@ -278,4 +278,9 @@ public abstract class AbstractHerobrine extends SkillUsingMob implements Stigmat
     public boolean canFreeze() {
         return false;
     }
+
+    @Override
+    public boolean canUsePortal(boolean allowPassengers) {
+        return false;
+    }
 }
