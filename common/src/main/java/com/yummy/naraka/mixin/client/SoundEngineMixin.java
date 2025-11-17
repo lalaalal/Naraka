75751061ff42409ace6ea473ce7f42665d8c982f
lalaalal <bontin0128@gmail.com>
2025-11-17 20:08:44 +0900

    fix: Boss music considers music volume setting

--- common/src/main/java/com/yummy/naraka/mixin/client/SoundEngineMixin.java
+++ common/src/main/java/com/yummy/naraka/mixin/client/SoundEngineMixin.java
@@ -3,9 +3,11 @@ package com.yummy.naraka.mixin.client;
 import com.yummy.naraka.client.sound.VolumeController;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
+import net.minecraft.client.Options;
 import net.minecraft.client.resources.sounds.SoundInstance;
 import net.minecraft.client.sounds.ChannelAccess;
 import net.minecraft.client.sounds.SoundEngine;
+import net.minecraft.util.Mth;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -19,12 +21,16 @@ public abstract class SoundEngineMixin implements VolumeController {
 
     @Shadow @Final private Map<SoundInstance, ChannelAccess.ChannelHandle> instanceToChannel;
 
+    @Shadow @Final private Options options;
+
     @Override
     public void naraka$setVolume(SoundInstance soundInstance) {
+        float maxVolume = options.getSoundSourceVolume(soundInstance.getSource());
+        float volume = Mth.lerp(soundInstance.getVolume(), 0, maxVolume);
         if (loaded) {
             ChannelAccess.ChannelHandle handle = instanceToChannel.get(soundInstance);
             if (handle != null)
-                handle.execute(channel -> channel.setVolume(soundInstance.getVolume()));
+                handle.execute(channel -> channel.setVolume(volume));
         }
     }
 }
