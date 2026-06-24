7f810cb4c0b8a1003345910ec412e0d6a9906dd0
lalaalal <bontin0128@gmail.com>
2026-06-24 17:48:28 +0900

    fix: Remove ComponentPredicateRecipe

--- common/src/main/java/com/yummy/naraka/world/item/crafting/NarakaRecipeSerializers.java
+++ common/src/main/java/com/yummy/naraka/world/item/crafting/NarakaRecipeSerializers.java
@@ -3,16 +3,14 @@ package com.yummy.naraka.world.item.crafting;
 import com.yummy.naraka.core.registries.HolderProxy;
 import com.yummy.naraka.core.registries.RegistryProxy;
 import net.minecraft.core.registries.Registries;
+import net.minecraft.world.Container;
 import net.minecraft.world.item.crafting.Recipe;
-import net.minecraft.world.item.crafting.RecipeInput;
 import net.minecraft.world.item.crafting.RecipeSerializer;
 
 import java.util.function.Supplier;
 
 public class NarakaRecipeSerializers {
-    public static final HolderProxy<RecipeSerializer<?>, RecipeSerializer<ComponentPredicateRecipe>> COMPONENT_PREDICATE_RECIPE = register("component_predicate_recipe", ComponentPredicateRecipe.Serializer::new);
-
-    private static <I extends RecipeInput, T extends Recipe<I>> HolderProxy<RecipeSerializer<?>, RecipeSerializer<T>> register(String name, Supplier<RecipeSerializer<T>> serializer) {
+    private static <C extends Container, T extends Recipe<C>> HolderProxy<RecipeSerializer<?>, RecipeSerializer<T>> register(String name, Supplier<RecipeSerializer<T>> serializer) {
         return RegistryProxy.register(Registries.RECIPE_SERIALIZER, name, serializer);
     }
 
--- common/src/main/java/com/yummy/naraka/world/item/crafting/NarakaRecipeTypes.java
+++ common/src/main/java/com/yummy/naraka/world/item/crafting/NarakaRecipeTypes.java
@@ -3,12 +3,12 @@ package com.yummy.naraka.world.item.crafting;
 import com.yummy.naraka.core.registries.HolderProxy;
 import com.yummy.naraka.core.registries.RegistryProxy;
 import net.minecraft.core.registries.Registries;
+import net.minecraft.world.Container;
 import net.minecraft.world.item.crafting.Recipe;
-import net.minecraft.world.item.crafting.RecipeInput;
 import net.minecraft.world.item.crafting.RecipeType;
 
 public class NarakaRecipeTypes {
-    private static <I extends RecipeInput, T extends Recipe<I>> HolderProxy<RecipeType<?>, RecipeType<T>> register(String name) {
+    private static <C extends Container, T extends Recipe<C>> HolderProxy<RecipeType<?>, RecipeType<T>> register(String name) {
         return RegistryProxy.register(Registries.RECIPE_TYPE, name, () -> new RecipeType<>() {
                     @Override
                     public String toString() {
