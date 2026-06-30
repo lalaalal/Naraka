bfdf6ea58da89ebf2c18b4ef32dbd83fd7cc1371
lalaalal <bontin0128@gmail.com>
2026-06-30 21:26:53 +0900

    feat: Change herobrine targeting policy

--- common/src/main/java/com/yummy/naraka/world/entity/Herobrine.java
+++ common/src/main/java/com/yummy/naraka/world/entity/Herobrine.java
@@ -44,6 +44,7 @@ import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.entity.*;
 import net.minecraft.world.entity.ai.attributes.Attributes;
 import net.minecraft.world.entity.ai.navigation.FlyingPathNavigation;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.entity.projectile.Projectile;
 import net.minecraft.world.entity.projectile.hurtingprojectile.Fireball;
 import net.minecraft.world.item.ItemStack;
@@ -199,6 +200,7 @@ public class Herobrine extends AbstractHerobrine {
         if (!cachedWatchingEntities.isEmpty()) {
             int randomSkip = random.nextInt(cachedWatchingEntities.size());
             cachedWatchingEntities.values().stream()
+                    .filter(livingEntity -> livingEntity instanceof Player)
                     .skip(randomSkip)
                     .findFirst()
                     .ifPresent(livingEntity -> selectedTarget = livingEntity);
