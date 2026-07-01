87225d49fa6f9e6936c1c26fdb7886bd10384d6b
lalaalal <bontin0128@gmail.com>
2026-07-01 15:36:28 +0900

    feat: Implement stun, add forge mixinextra

--- common/src/main/java/com/yummy/naraka/mixin/client/MinecraftMixin.java
+++ common/src/main/java/com/yummy/naraka/mixin/client/MinecraftMixin.java
@@ -1,27 +1,41 @@
 package com.yummy.naraka.mixin.client;
 
 import com.yummy.naraka.client.event.ClientEvents;
+import com.yummy.naraka.world.entity.data.StunHelper;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.player.LocalPlayer;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.ModifyArg;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import javax.annotation.Nullable;
 
 @Environment(EnvType.CLIENT)
 @Mixin(Minecraft.class)
 public abstract class MinecraftMixin {
+    @Shadow
+    @Nullable
+    public LocalPlayer player;
+
     @Inject(method = "destroy", at = @At("HEAD"))
     public void onMinecraftStopping(CallbackInfo ci) {
         ClientEvents.CLIENT_STOPPING.invoker().run(Minecraft.getInstance());
     }
 
-    @ModifyArg(method = "runTick", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/renderer/GameRenderer;render(FJZ)V"))
-    public float replacePartialTick(float partialTicks) {
-//        if (NarakaClientContext.TICK_FROZEN.getValue())
-//            return NarakaClientContext.FROZEN_PARTIAL_TICK.getValue();
-        return partialTicks;
+    @Inject(method = "startUseItem", at = @At("HEAD"), cancellable = true)
+    public void preventItemUseOnStun(CallbackInfo ci) {
+        if (player != null && StunHelper.isStun(player))
+            ci.cancel();
+    }
+
+    @Inject(method = "startAttack", at = @At("HEAD"), cancellable = true)
+    public void preventAttackOnStun(CallbackInfoReturnable<Boolean> cir) {
+        if (player != null && StunHelper.isStun(player))
+            cir.cancel();
     }
 }
