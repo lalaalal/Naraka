c16daea89b899d5e7b530d5616a5250becd4ad21
lalaalal <bontin0128@gmail.com>
2026-07-08 18:26:51 +0900

    fix: Stigma consume command English translation

--- common/src/main/java/com/yummy/naraka/commands/StigmaCommand.java
+++ common/src/main/java/com/yummy/naraka/commands/StigmaCommand.java
@@ -181,7 +181,7 @@ public class StigmaCommand {
                 succeed += 1;
             }
         }
-        Component component = Component.translatable(LanguageKey.STIGMA_COMMAND_CONSUME_KEY, succeed, cause.getName(), cause.getName());
+        Component component = Component.translatable(LanguageKey.STIGMA_COMMAND_CONSUME_KEY, succeed, cause.getName());
         source.sendSuccess(() -> component, false);
         return Command.SINGLE_SUCCESS;
     }
