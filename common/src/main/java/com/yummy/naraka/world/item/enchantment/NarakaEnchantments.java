4569fb1a6ede1d6195eee6b130c9922544fa310d
lalaalal <bontin0128@gmail.com>
2026-07-12 12:32:43 +0900

    fix: Trident item is impossible to enchant loyalty

--- common/src/main/java/com/yummy/naraka/world/item/enchantment/NarakaEnchantments.java
+++ common/src/main/java/com/yummy/naraka/world/item/enchantment/NarakaEnchantments.java
@@ -41,7 +41,7 @@ public class NarakaEnchantments {
                 Enchantments.LOYALTY,
                 Enchantment.enchantment(
                                 Enchantment.definition(
-                                        itemHolderGetter.getOrThrow(NarakaItemTags.SPEAR_ENCHANTABLE),
+                                        itemHolderGetter.getOrThrow(NarakaItemTags.LOYALTY_ENCHANTABLE),
                                         5,
                                         3,
                                         Enchantment.dynamicCost(12, 7),
