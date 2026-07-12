3498cab01523f1ae9f1a6c138f081f2e15da7f39
lalaalal <bontin0128@gmail.com>
2026-07-12 21:21:40 +0900

    fix: Assert not calling die on alive herobrine

--- common/src/main/java/com/yummy/naraka/world/entity/Herobrine.java
+++ common/src/main/java/com/yummy/naraka/world/entity/Herobrine.java
@@ -783,6 +783,8 @@ public class Herobrine extends AbstractHerobrine {
 
     @Override
     public void die(DamageSource damageSource) {
+        if (isAlive())
+            return;
         if (damageSource.getEntity() instanceof LivingEntity livingEntity)
             rewardChallenger(livingEntity);
         super.die(damageSource);
