1538e638dba30a5fd5db61caae4a133d9ab44543
lalaalal <bontin0128@gmail.com>
2026-07-14 16:16:25 +0900

    fix: Item tooltip event firing

--- common/src/main/java/com/yummy/naraka/mixin/client/ItemStackMixin.java
+++ common/src/main/java/com/yummy/naraka/mixin/client/ItemStackMixin.java
@@ -15,18 +15,21 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
+import javax.annotation.Nullable;
 import java.util.List;
 
 @Environment(EnvType.CLIENT)
 @Mixin(ItemStack.class)
 public abstract class ItemStackMixin implements DataComponentHolder {
     @Inject(method = "getTooltipLines", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/item/Item;appendHoverText(Lnet/minecraft/world/item/ItemStack;Lnet/minecraft/world/item/Item$TooltipContext;Ljava/util/List;Lnet/minecraft/world/item/TooltipFlag;)V", shift = At.Shift.AFTER))
-    public void addReinforcementTooltip(Item.TooltipContext tooltipContext, Player player, TooltipFlag tooltipFlag, CallbackInfoReturnable<List<Component>> cir, @Local List<Component> components) {
-        ItemEvents.ITEM_TOOLTIP_TOP.invoker().addToTooltip(this, tooltipContext, player, tooltipFlag, components::add);
+    public void addReinforcementTooltip(Item.TooltipContext tooltipContext, @Nullable Player player, TooltipFlag tooltipFlag, CallbackInfoReturnable<List<Component>> cir, @Local List<Component> components) {
+        if (player != null)
+            ItemEvents.ITEM_TOOLTIP_TOP.invoker().addToTooltip(this, tooltipContext, player, tooltipFlag, components::add);
     }
 
     @Inject(method = "getTooltipLines", at = @At(value = "FIELD", target = "Lnet/minecraft/core/component/DataComponents;UNBREAKABLE:Lnet/minecraft/core/component/DataComponentType;"))
-    public void addBlessedTooltip(Item.TooltipContext tooltipContext, Player player, TooltipFlag tooltipFlag, CallbackInfoReturnable<List<Component>> cir, @Local List<Component> components) {
-        ItemEvents.ITEM_TOOLTIP_BOTTOM.invoker().addToTooltip(this, tooltipContext, player, tooltipFlag, components::add);
+    public void addBlessedTooltip(Item.TooltipContext tooltipContext, @Nullable Player player, TooltipFlag tooltipFlag, CallbackInfoReturnable<List<Component>> cir, @Local List<Component> components) {
+        if (player != null)
+            ItemEvents.ITEM_TOOLTIP_BOTTOM.invoker().addToTooltip(this, tooltipContext, player, tooltipFlag, components::add);
     }
 }
