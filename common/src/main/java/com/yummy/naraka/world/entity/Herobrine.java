65a334c2988af140047de7b5d3736b783d603cfb
lalaalal <bontin0128@gmail.com>
2026-08-13 17:08:36 +0900

    feat: Herobrine locked health counting target
    - Only players and bosses

--- common/src/main/java/com/yummy/naraka/world/entity/Herobrine.java
+++ common/src/main/java/com/yummy/naraka/world/entity/Herobrine.java
@@ -329,8 +329,10 @@ public class Herobrine extends AbstractHerobrine {
 
     private float calculateLockedHealth() {
         double sum = 0;
-        for (LivingEntity livingEntity : cachedWatchingEntities.values())
-            sum += LockedHealthHelper.get(livingEntity);
+        for (LivingEntity livingEntity : cachedWatchingEntities.values()) {
+            if (livingEntity.is(ConventionalTags.Entities.BOSSES) || livingEntity.is(EntityTypeIds.PLAYER))
+                sum += LockedHealthHelper.get(livingEntity);
+        }
         return (float) sum;
     }
 
@@ -664,15 +666,6 @@ public class Herobrine extends AbstractHerobrine {
         }
     }
 
-    private float calculateHurtDamageLimitByLockedHealth() {
-        double sum = 0;
-        for (LivingEntity livingEntity : cachedWatchingEntities.values()) {
-            double lockedHealth = LockedHealthHelper.get(livingEntity);
-            sum += (lockedHealth / (livingEntity.getMaxHealth() + lockedHealth)) * 20;
-        }
-        return MAX_HURT_DAMAGE_LIMIT - (float) sum;
-    }
-
     private void updateHurtDamageLimit(ServerLevel level) {
         if (phaseManager.getCurrentPhase() < 3 && hurtDamageLimit > 1) {
             float damageLimitReduce = NarakaConfig.COMMON.herobrineHurtLimitReduce.getValue();
@@ -683,7 +676,7 @@ public class Herobrine extends AbstractHerobrine {
             }
         }
         if (getPhase() == 3) {
-            hurtDamageLimit = calculateHurtDamageLimitByLockedHealth();
+            hurtDamageLimit = Math.max(0, MAX_HURT_DAMAGE_LIMIT - calculateLockedHealth());
         }
     }
 
